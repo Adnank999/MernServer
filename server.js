bab61968d9cdbe6ce61c8d0a25d199555b151c0d
Adnank999 <adnanhossain4ever@gmail.com>
2024-09-14 10:39:38 +0600

    updated changes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+  res.json({"message":"Server running successfully"});
+})
 app.post('/save-drawing', async (req, res) => {
   const { elements, appState } = req.body;
 
